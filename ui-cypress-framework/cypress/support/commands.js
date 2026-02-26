8a1b9a16a26d99353a8412f1a3b3eb8f016b94c1
nyahsakeeva <nyahsakeeva@gmail.com>
2026-02-26 14:58:44 -0600

    Create commands.js

---
+++ ui-cypress-framework/cypress/support/commands.js
@@ -0,0 +1,7 @@
+// Custom Cypress commands go here
+
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get('button[type="submit"]').click();
+});
