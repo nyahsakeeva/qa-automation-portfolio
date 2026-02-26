85af300a67b204957aac1e1eab4bdbf54a06fe88
nyahsakeeva <nyahsakeeva@gmail.com>
2026-02-26 16:30:30 -0600

    use credentials fixture in login test

--- ui-cypress-framework/cypress/e2e/login.cy.js
+++ ui-cypress-framework/cypress/e2e/login.cy.js
@@ -1,12 +1,24 @@
 import LoginPage from '../pages/LoginPage';
 
 describe('Login functionality', () => {
+  let credentials;
+
+  before(() => {
+    cy.fixture('credentials').then((data) => {
+      credentials = data;
+    });
+  });
+
   beforeEach(() => {
     LoginPage.visit();
   });
 
   it('should not allow login with invalid credentials', () => {
-    LoginPage.login('invalidUser', 'invalidPassword');
+    LoginPage.login(
+      credentials.invalidUser.username,
+      credentials.invalidUser.password
+    );
+
     LoginPage.elements.errorMessage().should('be.visible');
   });
 });
